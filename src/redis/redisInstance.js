86626db836ced38d165e2502a0ddeb41b7568dfe
AmarnathEDV <152170585+AmarnathEDV@users.noreply.github.com>
2024-03-05 17:22:43 +0530

    Update redisInstance.js

--- src/redis/redisInstance.js
+++ src/redis/redisInstance.js
@@ -1,7 +1,7 @@
 const redis = require("redis");
 
 // Create Redis Client
-const redisClient = redis.createClient({url : "rediss://red-cnjfkf6n7f5s73f8lsl0:TjOXoeQ1iMiRDnKyynPDS7tipvAIsqZW@singapore-redis.render.com:6379"});
+const redisClient = redis.createClient(6379);
 
 // Event Handling
 redisClient.on("error", (err) => {
