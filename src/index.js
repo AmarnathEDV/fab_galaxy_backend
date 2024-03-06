526b646c41cde1ae08c20e672d6ad0ad99c350f2
AmarnathEDV <152170585+AmarnathEDV@users.noreply.github.com>
2024-03-06 20:32:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -718,6 +718,6 @@ redisClient.connect();
 
 redisClient.ping();
 
-const server = app.listen(5000, function () {
+const server = app.listen(5001, function () {
   console.log("Server is running on port 5000 ");
 });
